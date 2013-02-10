5f0b0134a93cf18e086215e2207e90b069d03ecf
Robert Read <rread@bitsoflight.com>
2013-02-09 23:06:48 -0800

    Add logging and daemon support.

--- dyner53.py
+++ dyner53.py
@@ -6,23 +6,45 @@ import argparse
 import logging
 import logging.handlers
 import daemon
+import time
 
 logger = logging.getLogger('dyner53')
-
-def init_logging():
-    console = logging.StreamHandler()
-    console.setLevel(logging.DEBUG)
-    logger.addHandler(console)
-
-    syslog = logging.handlers.SysLogHandler()
-    syslog.setLevel(logging.INFO)
-    logger.addHandler(syslog)
-
-
+logger.setLevel(logging.DEBUG)
 route53 = boto.connect_route53()
 
-def daemonize():
-    pass
+
+def sys_is_osx_lion():
+    import platform
+    if platform.platform()[0:6] == 'Darwin':
+        (major, minor, patch) = platform.mac_ver()[0].split(".")
+        if major == '10' and minor in ['7', '8']:
+            return True
+    return False
+
+
+def init_logging(daemon = False):
+    """
+
+    :param daemon:
+    """
+    console = logging.StreamHandler()
+    console.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(name)s[%(process)d]: %(message)s')
+    console.setFormatter(formatter)
+    logger.addHandler(console)
+
+    if daemon:
+        # workaround MacPorts bug #37990
+        if sys_is_osx_lion():
+            syslog_address = '/var/run/syslog'
+        else:
+            syslog_address = ('localhost', logging.handlers.SYSLOG_UDP_PORT)
+        syslog = logging.handlers.SysLogHandler(address=syslog_address,
+                                                facility='daemon')
+        syslog.setFormatter(formatter)
+        syslog.setLevel(logging.INFO)
+        logger.addHandler(syslog)
+
 
 def get_args():
     parser = argparse.ArgumentParser()
@@ -31,7 +53,6 @@ def get_args():
     parser.add_argument('--ip',
                         help='Set domain to specified IP instead of current public address',
                         default=None)
-    parser.add_argument('--daemon', help="run as a daemon", action="store_true")
     subparsers = parser.add_subparsers()
 
     parser_check = subparsers.add_parser("check")
@@ -42,6 +63,9 @@ def get_args():
 
     parser_update = subparsers.add_parser('delete')
     parser_update.set_defaults(func=do_delete)
+
+    parser_daemon = subparsers.add_parser('daemon')
+    parser_daemon.set_defaults(func=do_daemon)
     args = parser.parse_args()
     return args
 
@@ -56,12 +80,13 @@ def get_dyn_domain(sub, domain):
 
 
 def do_check(args):
+    init_logging()
     dyn_domain = get_dyn_domain(args.subdomain, args.domain)
     my_ip = args.ip or get_public_ip()
     zone = route53.get_zone(args.domain)
     rec = zone.get_a(dyn_domain)
     if rec is not None:
-        print ("Current IP is %s, %s = %s" % (my_ip, dyn_domain, rec.resource_records))
+        logger.info("Current IP is %s, %s = %s" % (my_ip, dyn_domain, rec.resource_records))
         if my_ip in rec.resource_records:
             logger.debug("Dynamic address is current.")
         else:
@@ -71,6 +96,10 @@ def do_check(args):
 
 
 def do_update(args):
+    init_logging()
+    update(args)
+
+def update(args):
     dyn_domain = get_dyn_domain(args.subdomain, args.domain)
     my_ip = args.ip or get_public_ip()
     zone = route53.get_zone(args.domain)
@@ -78,13 +107,14 @@ def do_update(args):
     if rec is None:
         zone.add_a(dyn_domain, my_ip, ttl=60)
     elif my_ip not in rec.resource_records:
-        logger.critical('Updating %s from %s -> %s' % (dyn_domain, rec.resource_records, my_ip))
+        logger.warn('updating %s from %s -> %s' % (dyn_domain, rec.resource_records, my_ip))
         zone.update_a(dyn_domain, my_ip, ttl=60)
     else:
-        logger.debug("No update needed.")
+        logger.info("No update needed.")
 
 
 def do_delete(args):
+    init_logging()
     dyn_domain = get_dyn_domain(args.subdomain, args.domain)
     zone = route53.get_zone(args.domain)
     rec = zone.get_a(dyn_domain)
@@ -94,13 +124,31 @@ def do_delete(args):
         zone.delete_a(rec.name, rec.identifier)
 
 
+def do_daemon(args):
+    """
+
+    :param args:
+    """
+
+    with daemon.DaemonContext():
+        init_logging(True)
+        run_daemon(args)
+
+def run_daemon(args):
+    global route53
+    route53.close()
+    logger.warn("Dynamic route53 updater started for: %s" %
+                get_dyn_domain(args.subdomain, args.domain))
+    while True:
+        route53 = boto.connect_route53()
+        update(args)
+        route53.close()
+        time.sleep(300)
+
+
 def main():
     args = get_args()
-    if args.daemon:
-        daemonize()
-    init_logging()
     args.func(args)
-
 
 if __name__ == '__main__':
     main()
